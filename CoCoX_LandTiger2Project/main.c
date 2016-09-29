f5915d56d624dc54f921e27f713ed6d7b829eccd
Unknown <um.nix.user@gmail.com>
2016-09-29 19:54:12 +0400

    I think we should forgot about CoCoX LandTiger2 project, i decided to use Eclipse
    I think we should forgot about CoCoX LandTiger2 project, i decided to use Eclipse

--- CoCoX_LandTiger2Project/main.c
+++ CoCoX_LandTiger2Project/main.c
@@ -1,6 +1,42 @@
+#include "main.h"
+#include "data/networkConfig.h"
+#include "usr/networkManager.h"
+
+/* Library includes. */
+#include "LPC17xx.h"
+#include "lpc17xx_emac.h"
+#include "lpc17xx_gpio.h"
+
+/* lwIP includes. */
+#include "lwip/tcpip.h"
+
 
 int main(void)
 {
+	// todo: umv: read config from flash
+	struct NetworkConfig networkConfig;
+	networkConfig._ipV4Address._byte1 = DEFAULT_IP_BYTE1;
+	networkConfig._ipV4Address._byte2 = DEFAULT_IP_BYTE2;
+	networkConfig._ipV4Address._byte3 = DEFAULT_IP_BYTE3;
+	networkConfig._ipV4Address._byte4 = DEFAULT_IP_BYTE4;
+
+	networkConfig._networkMask._byte1 = DEFAULT_NETMASK_BYTE1;
+	networkConfig._networkMask._byte2 = DEFAULT_NETMASK_BYTE2;
+	networkConfig._networkMask._byte3 = DEFAULT_NETMASK_BYTE3;
+	networkConfig._networkMask._byte4 = DEFAULT_NETMASK_BYTE4;
+
+	networkConfig._defaultGateway._byte1 = DEFAULT_GATEWAY_BYTE1;
+	networkConfig._defaultGateway._byte2 = DEFAULT_GATEWAY_BYTE2;
+	networkConfig._defaultGateway._byte3 = DEFAULT_GATEWAY_BYTE3;
+	networkConfig._defaultGateway._byte4 = DEFAULT_GATEWAY_BYTE4;
+
+	// modules initialization ...
+	// 1. Init clocks
+	// 2. Init IO ports
+	// 3. Init other peripherials
+	// 4. Init network settings
+
+    initNetworks(&networkConfig);
 
     while(1)
     {
---
+++ CoCoX_LandTiger2Project/usr/networkManager.c
@@ -0,0 +1,28 @@
+#include "networkManager.h"
+#include "lwip/tcpip.h"
+#include "lwip/netif.h"
+#include "netif/ethernetif.h"
+
+
+void initNetworks(struct NetworkConfig* config)
+{
+    initEthernetChip();
+    initLwIp(config);
+}
+
+void initEthernetChip()
+{
+     // LPC1768 contains onboard ethernet chip, therefore this method is empty.
+}
+
+void initLwIp(struct NetworkConfig* config)
+{
+	struct netif xnetif;
+    // ip_addr_t ipAddress;
+    // ip_addr_t networkMask;
+    // ip_addr_t gateway;
+
+    tcpip_init(NULL, NULL);
+    //IP4_ADDR(&ipAddress, config->_ipV4Address._byte1,);
+    netif_add(&xnetif, &config->_ipV4Address._dword, &config->_networkMask._dword, &config->_defaultGateway._dword, NULL, &ethernetif_init, &tcpip_input);
+}
